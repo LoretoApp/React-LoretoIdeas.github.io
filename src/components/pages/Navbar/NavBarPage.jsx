c30caf3e67562ea39a414b9539d7c11ca8254778
Annydrn <andrea.duran.cabana@gmail.com>
2023-07-31 19:42:17 -0400

    creacion link producto route

--- src/components/pages/Navbar/NavBarPage.jsx
+++ src/components/pages/Navbar/NavBarPage.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Link} from "react-router-dom"
 
 function NavBarPage() {
   return (
@@ -14,6 +15,8 @@ function NavBarPage() {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <Link to="productos" >Productos</Link>
+
               <NavDropdown.Item href="productos">Productos</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
