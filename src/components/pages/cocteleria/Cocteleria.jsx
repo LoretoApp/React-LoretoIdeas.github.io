752f18c4497866eca115e579a37614ea15baebf1
Juan Nicolas <juan_nicolaso@live.com>
2023-08-07 10:29:37 -0400

    arregle cocteeleria y nosotros

--- src/components/pages/cocteleria/Cocteleria.jsx
+++ src/components/pages/cocteleria/Cocteleria.jsx
@@ -13,8 +13,8 @@ function Cocteleria() {
     <section>
       <div className="container-fluid text-center1 fondo">
         <div className="container-fluid">
-          <p className='titulo'>Cocteleria</p>
-          <p className='parrafo'>Damos servicios de banquetería para distintos eventos privados o familiares.<br/>
+          <p className='cocteleria'>Cocteleria</p>
+          <p className='parrafoCocteleria'>Damos servicios de banquetería para distintos eventos privados o familiares.<br/>
             Se pueden crear menús salados, dulces o mixtos, según lo que necesite el cliente.<br/>
             Se debe hacer la solicitud con al menos 10 días de anticipación.<br/>
             Puede escribirnos al WhatsApp para más consultas.</p>
--- src/components/pages/desayunos/Desayunos.jsx
+++ src/components/pages/desayunos/Desayunos.jsx
@@ -1,7 +1,7 @@
 import Footer from "../Footer/Footer"
 import NavBarPage from "../navbar/NavBarPage"
 import CarruselDesayunos from "./CarruselDesayunos"
-
+import "./desayunos.css"
 
 
 function Desayunos() {
@@ -12,8 +12,8 @@ function Desayunos() {
     <section id = "fondo">
       <div className="container-fluid text-center1 fondo">
         <div className="container-fluid">
-          <p className="titulo">Desayunos</p>
-          <p className="parrafo">Damos servicios de banquetería para distintos eventos privados o familiares.<br/>
+          <p className="desayunos">Desayunos</p>
+          <p className="parrafoDesayuno">Damos servicios de banquetería para distintos eventos privados o familiares.<br/>
             Se pueden crear menús salados, dulces o mixtos, según lo que necesite el cliente.<br/>
             Se debe hacer la solicitud con al menos 10 días de anticipación.<br/>
             Puede escribirnos al WhatsApp para más consultas.</p>
