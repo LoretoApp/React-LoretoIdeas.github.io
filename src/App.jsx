b164967833643a3634b5cf4285cfdaeb9241aa29
Nicolecuadrav <nicolecuadravenegas@gmail.com>
2023-08-16 16:49:00 -0400

    arreglor formularios administrador

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,14 @@ import AppLogin from "./components/login/AppLogin";
 import { AuthProvider } from "./controllers/Atentificacion/context/AuthContext";
 import VistaAdministrador from "./components/pages/admin/admin";
 import CrearTartaleta from "./components/pages/admin/Tartaletas/CrearTartaleta";
+import ModificarFullTartaleta from "./components/pages/admin/Tartaletas/ModificarFullTartaleta";
+import ModificarStatusTartaleta from "./components/pages/admin/Tartaletas/ModificarStatusTartaleta";
+import MostrarTartaletas from "./components/pages/admin/Tartaletas/MostrarTartaletas";
+import CrearTorta from './components/pages/admin/Tortas/CrearTorta';
+import ModificarFullTorta from './components/pages/admin/Tortas/ModificarFullTorta';
+import ModificarStatusTorta from './components/pages/admin/Tortas/ModificarStatusTorta';
+import MostrarTorta from './components/pages/admin/Tortas/MostrarTorta';
+
 
 function App() {
   return (
@@ -24,6 +32,13 @@ function App() {
           <Route path="administrador" element={<VistaAdministrador/>}/>
           <Route path="login" element={<AppLogin />} />
           <Route path="administrador1" element={<CrearTartaleta/>}/>
+          <Route path="administrador2" element={<ModificarFullTartaleta/>}/>
+          <Route path="administrador3" element={<ModificarStatusTartaleta/>}/>
+          <Route path="administrador4" element= {<MostrarTartaletas/>}/> 
+          <Route path="administrador5" element= {<CrearTorta/>}/> 
+          <Route path="administrador6" element= {<ModificarFullTorta/>}/>
+          <Route path="administrador7" element= {<ModificarStatusTorta/>}/>
+          <Route path="administrador8" element= {<MostrarTorta/>}/>
           
         </Routes>
       </AuthProvider>
