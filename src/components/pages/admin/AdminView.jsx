4f50934f00261012d87809339dcaff7b353428af
camilo-OG <camilo.osorio.gsach.cl>
2023-08-17 19:25:10 -0400

    inicio

--- src/components/pages/admin/AdminView.jsx
+++ src/components/pages/admin/AdminView.jsx
@@ -188,7 +188,7 @@ export default function AdminView() {
       {verTortaDesactivar && <ModificarStatusTorta/>}
       {verTartaletaDesactivar && <ModificarStatusTartaleta/>}
       {verTortaAgregar && <CrearTorta/>}
-      {verTartaletaAgregar && <CrearTartaleta/>}     
+      {verTartaletaAgregar && <CrearTartaleta/>}      
       
     </div>
 
