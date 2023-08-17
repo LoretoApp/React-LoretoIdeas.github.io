833e9f0054e3522f48dd5a486e0f80006d882a2a
camilo-OG <camilo.osorio.gsach.cl>
2023-08-17 17:21:53 -0400

    funcionan las rutas de administrador

--- src/components/pages/admin/FuncionesAdmin.jsx
+++ src/components/pages/admin/FuncionesAdmin.jsx
@@ -1,10 +1,24 @@
-
+import CrearTartaleta from "./Tartaletas/CrearTartaleta";
+import ModificarStatusTartaleta from "./Tartaletas/ModificarStatusTartaleta";
+import ModificarFullTartaleta from "./Tartaletas/ModificarFullTartaleta";
+import MostrarTartaletas from "./Tartaletas/MostrarTartaletas";
+import CrearTorta from "./Tortas/CrearTorta";
+import ModificarFullTorta from "./Tortas/ModificarFullTorta";
+import ModificarStatusTorta from "./Tortas/ModificarStatusTorta";
+import MostrarTorta from "./Tortas/MostrarTorta";
+import { useState } from "react";
 
 export default function FuncionesAdmin() {
-  return (
-    <section id='funcionesAdmin'>
-      
+  const [verTorta, setVerTorta] = useState(false)
+  
+  
+  
 
+  return (
+    <section className="funcionesAdmin" id='funcionesAdmin'>
+      {verTorta && <MostrarTorta/>}
+      
+      
     </section>
   )
 }
