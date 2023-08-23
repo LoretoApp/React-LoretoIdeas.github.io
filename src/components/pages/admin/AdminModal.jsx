14459186384acaffe6bf5801fe511c75d5668381
camilo-OG <camilo.osorio.gsach.cl>
2023-08-23 15:29:25 -0400

    merge rama camilo

--- src/components/pages/admin/AdminModal.jsx
+++ src/components/pages/admin/AdminModal.jsx
@@ -14,11 +14,11 @@ export default function AdminModal({accion, show, handleClose, funcion1,funcion2
           <div className="contenedor-boton-admin">
             <button className='btn boton-torta-modal'onClick={funcion1} >
               <h4 className='titulo-boton-modal'>Torta</h4>
-              <img src="img/torta2.jpg" alt="torta de fresa" className='img-modal-admin'/>
+              <img src="img/img-sinFondo/imagenAdmin2.png" alt="torta de fresa" className='img-modal-admin'/>
             </button>
             <button className='btn boton-tartaleta-modal' onClick={funcion2}>
               <h4 className='titulo-boton-modal'>Tartaleta</h4>
-              <img src="img/pie-limon.PNG" alt="torta de fresa" className='img-modal-admin'/>
+              <img src="img/img-sinFondo/imagenAdmin1.png" alt="torta de fresa" className='img-modal-admin'/>
             </button>
           </div>
         </div>
