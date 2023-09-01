4f5decbd57e75b9e1070934ff056af9a8e80a236
Annydrn <andrea.duran.cabana@gmail.com>
2023-09-01 04:29:06 -0400

    actualizacion admin

--- src/components/pages/nosotros/CarruselNosotros1.jsx
+++ src/components/pages/nosotros/CarruselNosotros1.jsx
@@ -3,7 +3,7 @@ import "./carruselnosotros.css"
 function CarruselNosotros1() {
   return (
     <div className="carruselNosotros">
-    <img src="img/bannerNosotros.png" alt="Imagen 1" className="img-fluid"/>
+    <img src="img\bannerNosotros.png" alt="Imagen 1" className="img-fluid"/>
 
   </div>
   );
