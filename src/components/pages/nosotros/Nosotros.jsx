0e07a208c960fd595ba10ac1a3644065932277dd
Juan Nicolas <juan_nicolaso@live.com>
2023-09-04 11:02:43 -0300

    CAMBIO DE NUMERO DE WSP NO SPAMEAR

--- src/components/pages/nosotros/Nosotros.jsx
+++ src/components/pages/nosotros/Nosotros.jsx
@@ -60,7 +60,7 @@ function Nosotros() {
       </section>
       <Link
         className="whatsapp-float"
-        to="https://api.whatsapp.com/send?phone=56975688406&text=Hola%21%20Me%20gustar%C3%ADa%20saber%20m%C3%A1s%20de%20Loreto%20Ideas"
+        to="https://api.whatsapp.com/send?phone=56932939888&text=Hola%21%20Me%20gustar%C3%ADa%20saber%20m%C3%A1s%20de%20Loreto%20Ideas"
         target="_blank"
         rel="noopener noreferrer"
       >
--- src/components/pages/productos/Productos.jsx
+++ src/components/pages/productos/Productos.jsx
@@ -11,7 +11,7 @@ function Productos () {
   return (
   <div className="productos productos-fondo">
 
-  <Link className="whatsapp-float" to="https://api.whatsapp.com/send?phone=56975688406&text=Hola%21%20Quisiera%20consultar%20por%20un%20producto"
+  <Link className="whatsapp-float" to="https://api.whatsapp.com/send?phone=56932939888&text=Hola%21%20Quisiera%20consultar%20por%20un%20producto"
  target="_blank" rel="noopener noreferrer">
     <img className="whatsapp" src="icons\wspp.png" alt="whatsapp"/>
   </Link>
